20c76a6637ef6b0414e645af05ae0d4e78c71fad
ranaxdev <sharrq1@gmail.com>
2021-07-13 06:06:58 +0500

    cmake init

---
+++ cmake-build-debug/CMakeFiles/3Dconics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3Dconics"
+  "3Dconics.pdb"
+  "CMakeFiles/3Dconics.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3Dconics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
