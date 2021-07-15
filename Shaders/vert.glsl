bd7e30e858783e2f942085bc747c47ad40a16a24
ranaxdev <sharrq1@gmail.com>
2021-07-15 23:16:48 +0500

    Shader program test

--- Shaders/vert.glsl
+++ Shaders/vert.glsl
@@ -1,5 +1,5 @@
 #version 450 core
 
 void main(void){
-    gl_Position = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    gl_Position = vec4(0.5f, 0.5f, 0.5f, 1.0f);
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,26 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include "Harness.h"
 
+#include "Harness.h"
+#include "Shader.h"
+
+#define SHADER_SRC "/home/rana/Desktop/3Dconics/Shaders"
 class App : public conics::Harness{
+
 public:
+    void startup() override {
+        Shader* shader = new Shader(SHADER_SRC"/vert.glsl", SHADER_SRC"/frag.glsl");
+        shader->bind();
+
+        GLuint VAO;
+        glCreateVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+    }
+
     void render(float currentTime) override {
+        glPointSize(40.0f);
+        glDrawArrays(GL_POINTS, 0, 1);
     }
 };
 
