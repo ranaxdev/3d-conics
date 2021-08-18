72b0475cb4272b6c0eaba44268daa3e0d19d3924
ranaxdev <sharrq1@gmail.com>
2021-08-18 05:45:15 +0500

    Dependencies refactor

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,9 @@
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
 
 #include <cmath>
